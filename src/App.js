c191d89ae8fe8da101df86795ece1acf8b72dec5
Jacob Greenway <jagreenway12@gmail.com>
2018-03-23 18:16:22 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+import Driver from './Driver';
+import Commander from './Commander';
+import Navigator from './Navigator';
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path='/driver' component={Driver} />
+        <Route path='/commander' component={Commander} />
+        <Route path='/navigator' component={Navigator} />
+      </Switch>
+    );
+  }
+}
+
+export default App;
