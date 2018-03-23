b3ddb0e263340672bc96517358313c58e1143dc5
Jacob Greenway <jagreenway12@gmail.com>
2018-03-23 18:53:37 -0500

    added map

--- src/Driver.js
+++ src/Driver.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import map from './map.png';
 
 export default class Driver extends Component {
   render() {
-    return(
-      <div />
+    return (
+      <div>
+        <div className='Map' />
+        <div className='Controls' />
+      </div>
     );
   }
 }
