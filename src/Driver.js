1837425f341991db6c8a1208d2ee7cb9520b1ec5
Jacob Greenway <jagreenway12@gmail.com>
2018-03-23 20:28:17 -0500

    slider added

--- src/Driver.js
+++ src/Driver.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import wheel from './wheel.png';
+import Slider from 'react-rangeslider';
+import 'react-rangeslider/lib/index.css'
 
 export default class Driver extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    }
+  }
+
+  handleChange = value => {
+    this.setState({
+      value: value
+    });
+  }
+
   render() {
     return (
       <div>
-        <div className='Map' />
+        <div className='Map'/>
         <div className='Controls'>
           <div className='Speed'>
-
+            <Slider
+              orientation='vertical'
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
           </div>
           <div className='Wheel'>
             <img src={wheel} />
